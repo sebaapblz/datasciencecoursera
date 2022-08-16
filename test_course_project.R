0ea71367cf4756f603c1542cb5e3bec803e9e277
sebaapblz <86484125+sebaapblz@users.noreply.github.com>
2022-08-15 21:51:22 -0400

    Third commit

--- test_course_project.R
+++ test_course_project.R
@@ -1,1 +1,3 @@
 print("This file was made to approve The Data Scientist's Toolbox course")
+print("Also you can see this on the repository that I created by copying and pasting this link:")
+print("https://github.com/sebaapblz/test-RStudio")
